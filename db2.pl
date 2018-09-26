9f148385142e509a3664f550fb85c5c0bbc2f815
Vincius Couto Espindola <vinicius.c.e@hotmail.com>
2018-09-26 18:19:36 -0300

    Node find, abb test.

--- db2.pl
+++ db2.pl
@@ -1,5 +1,15 @@
-tree(bst(7,bst(3,bst(2,n,n),bst(5,n,n)),bst(11,bst(9,n,n),bst(13,n,n)))).
+t(7,t(3,t(2,n,n),t(5,n,n)),t(11,t(9,n,n),t(13,n,n))).
 
-findNode(X,bst(X,_,_)).
-findNode(X,bst(N,L,_)) :- X < N, findNode(X,L).
-findNode(X,bst(N,_,R)) :- X > N, findNode(X,R).
+findNode(X,t(X,_,_)).
+findNode(X,t(N,L,_)) :- X < N, findNode(X,L).
+findNode(X,t(N,_,R)) :- X > N, findNode(X,R).
+
+isBst(t(N,L,R)) :- checkBstL(N,L), checkBstR(N,R).
+checkBstL(X,n).
+checkBstL(X,t(N,L,R)) :- X > N, checkBstL(N,L), checkBstR(N,R).
+checkBstR(X,n).
+checkBstR(X,t(N,L,R)) :- X < N, checkBstL(N,L), checkBstR(N,R).
+
+addNode(N,n,E) :- addNode(N,R).
+addNode(N,t(X,L,R)) :- N < X, addNode(N,L).
+addNode(N,t(X,L,R)) :- N > X, addNode(N,R).
