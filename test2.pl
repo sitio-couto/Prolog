f3deecb7e712ec719ad7b18663dd06b0e06122eb
Vincius Couto Espindola <vinicius.c.e@hotmail.com>
2018-10-04 16:42:33 -0300

    Fixed several

--- test2.pl
+++ test2.pl
@@ -2,19 +2,20 @@
 % insereabb(+Chave,+Valor, +ArvVelha, -ArvNova)
 % trocaabb(+Chave, +Valoe, +ArvVelha, -ArvNova)
 % lookupabb(+Chave, +Arvore, -Valor)
+% arv(11,11,arv(9,9,arv(7,7,arv(3,3,arv(2,2,vazia,vazia),arv(5,5,vazia,vazia)),vazia),vazia),arv(13,13,vazia,vazia))
 
 lookupabb(K,arv(K,V,_,_),V).
 lookupabb(K,arv(N,_,L,_),S) :- K < N, lookupabb(K,L,S).
 lookupabb(K,arv(N,_,_,R),S) :- K > N, lookupabb(K,R,S).
 
 trocaabb(K,V,arv(K,Y,L,R),E) :- E = arv(K,V,L,R).
-trocaabb(K,V,arv(X,Y,L,R),E) :- N < K, trocaabb(K,V,L,Z), E = arv(X,Y,Z,R).
-trocaabb(K,V,arv(X,Y,L,R),E) :- N > K, trocaabb(K,V,R,Z), E = arv(X,Y,L,Z).
+trocaabb(K,V,arv(X,Y,L,R),E) :- X > K, trocaabb(K,V,L,Z), E = arv(X,Y,Z,R).
+trocaabb(K,V,arv(X,Y,L,R),E) :- X < K, trocaabb(K,V,R,Z), E = arv(X,Y,L,Z).
 
 insereabb(K,V,vazia,E) :- E = t(K,V,vazia,vazia).
 insereabb(K,V,arv(K,Y,L,R),E) :- false.
-insereabb(K,V,arv(X,Y,L,R),E) :- N < K, insereabb(K,V,L,Z), E = t(X,Y,Z,R).
-insereabb(K,V,arv(X,Y,L,R),E) :- N > K, insereabb(K,V,R,Z), E = t(X,Y,L,Z).
+insereabb(K,V,arv(X,Y,L,R),E) :- X > K, insereabb(K,V,L,Z), E = t(X,Y,Z,R).
+insereabb(K,V,arv(X,Y,L,R),E) :- X < K, insereabb(K,V,R,Z), E = t(X,Y,L,Z).
 
 % getdic(+Chave, +Dicionario, -Valor)
 % xgetdic(+Chave, +Dicionario, +ValorDefault, -Valor)
